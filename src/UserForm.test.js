5d2cbf3dd6e179bfac0ecf67cf317e152002603f
Pavel Smolov <plexsmol@gmail.com>
2023-05-01 21:06:42 -0400

    chore: recording function call

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -16,25 +16,33 @@ test('it shows two inputs and a button', () => {
   expect(button).toBeInTheDocument();
 });
 
-test("it calls onUserAdd when the form is submitted", async () => {
+test('it calls onUserAdd when the form is submitted', () => {
   // NOT THE BEST IMPLEMENTATION
+  const argList = [];
+  const callback = (...args) => {
+    argList.push(args);
+  };
   // Try to render my component
-  render(<UserForm />);
+  render(<UserForm onUserAdd={callback} />);
 
   // Find the two inputs
-  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  const [nameInput, emailInput] = screen.getAllByRole('textbox');
 
   // Simulate typing in a name
-  await user.click(nameInput);
-  await user.keyboard("jane");
+  user.click(nameInput);
+  user.keyboard('jane');
 
   // Simulate typing in an email
-  await user.click(emailInput);
-  await user.keyboard("jane@jane.com");
+  user.click(emailInput);
+  user.keyboard('jane@jane.com');
 
   // Find the button
+  const button = screen.getByRole('button');
 
   // Simulate clicking the button
+  user.click(button);
 
   // Assertion to make sure 'onUserAdd' gets called with email/name
+  expect(argList).toHaveLength(1);
+  expect(argList[0][0]).toEqual({ name: 'jane', email: 'jane@jane.com' });
 });
