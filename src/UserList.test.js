171511b31d7cb71f47a03ab0530318c22b8eb92a
Pavel Smolov <plexsmol@gmail.com>
2023-05-03 20:39:46 -0400

    chore: query function escape hatches

---
+++ src/UserList.test.js
@@ -0,0 +1,20 @@
+import { render, screen, within } from '@testing-library/react';
+import UserList from './UserList';
+
+test('render one row per user', () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' }
+  ]
+  render(<UserList users={users} />);
+
+  // Find all the rows in the table
+  const rows = within(screen.getByTestId('users')).getAllByRole('row');
+
+  //Assertions: correct number of rows in the table
+  expect(rows).toHaveLength(2);
+
+})
+
+test('render the email and name of each user', () => { })
+
