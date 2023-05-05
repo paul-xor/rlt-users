db863de1500fa890fdd11e1d3de078557951f391
Pavel Smolov <plexsmol@gmail.com>
2023-05-04 20:24:43 -0400

    chore: avoiding beforeeach, remove code duplication

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,12 +1,20 @@
 import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
 
-test('render one row per user', () => {
+function renderComponent() {
   const users = [
     { name: 'jane', email: 'jane@jane.com' },
     { name: 'sam', email: 'sam@sam.com' }
   ]
   render(<UserList users={users} />);
+
+  return {
+    users,
+  }
+}
+
+test('render one row per user', () => {
+  renderComponent();
 
   // Find all the rows in the table
   const rows = within(screen.getByTestId('users')).getAllByRole('row');
@@ -17,11 +25,7 @@ test('render one row per user', () => {
 })
 
 test('render the email and name of each user', () => {
-  const users = [
-    { name: 'jane', email: 'jane@jane.com' },
-    { name: 'sam', email: 'sam@sam.com' }
-  ]
-  render(<UserList users={users} />);
+  const { users } = renderComponent();
 
   for (let user of users) {
     const name = screen.getByRole('cell', { name: user.name });
