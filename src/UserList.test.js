b73e634797b23185446a90dea6f6e5aa2cdecfc1
Pavel Smolov <plexsmol@gmail.com>
2023-05-04 20:17:43 -0400

    chore: testing table contents

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -16,5 +16,19 @@ test('render one row per user', () => {
 
 })
 
-test('render the email and name of each user', () => { })
+test('render the email and name of each user', () => {
+  const users = [
+    { name: 'jane', email: 'jane@jane.com' },
+    { name: 'sam', email: 'sam@sam.com' }
+  ]
+  render(<UserList users={users} />);
+
+  for (let user of users) {
+    const name = screen.getByRole('cell', { name: user.name });
+    const email = screen.getByRole('cell', { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  };
+})
 
